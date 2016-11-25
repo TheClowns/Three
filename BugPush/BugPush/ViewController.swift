f5fc659d86e66013ea46d0744b67d3f9a35148d6
cuijinlei <1142382088@qq.com>
2016-11-25 20:12:35 +0800

    初始化项目

---
+++ BugPush/BugPush/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BugPush
+//
+//  Created by 这是C先生 on 2016/11/24.
+//  Copyright © 2016年 这是C先生. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
